54e23f2de0214531433b73bed2dd47acad7f9204
Aaron <aaronkh19@gmail.com>
2020-09-07 21:25:07 -0600

    finished projects page for now

--- src/components/projects.js
+++ src/components/projects.js
@@ -10,6 +10,7 @@ import {
   Button,
   CardMenu,
   IconButton,
+  CardText,
 } from "react-mdl";
 
 class Projects extends Component {
@@ -21,15 +22,60 @@ class Projects extends Component {
   toggleCategories() {
     if (this.state.activeTab === 0) {
       return (
-        <div>
-          <h1>This is React</h1>
-        </div>
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover",
+            }}>
+            React Project 3
+          </CardTitle>
+          <CardText>Coming soon..</CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>Demo</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name='share'></IconButton>
+          </CardMenu>
+        </Card>
       );
     } else if (this.state.activeTab === 1) {
       return (
-        <div>
-          <h1>This is Express</h1>
-        </div>
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#000000",
+              height: "176px",
+              background:
+                "url(https://images.creativemarket.com/0.1.0/ps/5588184/300/200/m2/fpnw/wm0/511-.jpg?1545240045&s=7ecbeb09cbc5a0b65854f41f5ce6d0bb) center / cover",
+            }}>
+            Burger App
+          </CardTitle>
+          <CardText>
+            Node/Express Handlebars based application. Checkout my repo for the
+            code, also hit the demo link for a functioning app
+          </CardText>
+          <CardActions border>
+            <Button
+              href='https://github.com/Errn17/burger'
+              target='_blank'
+              colored>
+              Github
+            </Button>
+            <Button
+              href='https://infinite-garden-65417.herokuapp.com/'
+              target='_blank'
+              colored>
+              Demo
+            </Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name='share'></IconButton>
+          </CardMenu>
+        </Card>
       );
     } else if (this.state.activeTab === 2) {
       return (
